ea15244bf571bbda0ba949bea14ed346c687a072
Diana Morales <morales.02.cast@gmail.com>
2024-05-27 19:42:06 -0500

    se sube cambios de error

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,11 +1,21 @@
-// Error.js
 
-// Define la vista de error
 const ErrorView = () => {
-  const element = document.createElement('div');
-  element.textContent = 'Error Page';
+  const element = document.createElement("div");
+  element.innerHTML = `
+  <div id="grupo-error">
+  <div class="harrypotter">
+    <img class="imgError" src="img/error.jpg" alt="Fondo de error">
+  </div>
+  <div class="cuadro">
+    <p class="mensaje-error">
+      <h3>La página a la que ingresaste no es la correcta, verifica que el enlace sea correcto.</h3>
+    </p>
+  </div>
+</div>
+
+  `;
   return element;
 };
-  
+
 // Exporta ErrorView como valor predeterminado
 export default ErrorView;
